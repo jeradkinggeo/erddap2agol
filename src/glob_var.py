097e83aaee118109762eb889548196eb7d1aadd4
jeradkinggeo <jeradkinggeo@gmail.com>
2024-08-13 11:43:42 -0500

    Added data dictionary, working on getting working requests

---
+++ src/glob_var.py
@@ -0,0 +1,8 @@
+#This dictionary is how we will keep track of the urls and item ids for a given dataset
+DataDictionary = {
+    "Dataset1": {
+        "DataName": "Dataset1",
+        "ERDDAPUrl": "https://erddap.gcoos.org/erddap/tabledap/gcoos_42G01.htmlTable?time%2Clatitude%2Clongitude%2Cplatform%2Ccrs%2Cdepth%2Cwater_temperature_instrument_0%2Cocean_currents_instrument_0%2Csea_surface_temperature_0&time%3E=2024-08-06T00%3A00%3A00Z&time%3C=2024-08-10T13%3A36%3A00Z",
+        "ItemID": "insert item id here"
+        }
+}
