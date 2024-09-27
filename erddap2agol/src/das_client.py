39d5242018b396799d60c5b14279a7da0df939de
jeradkinggeo <jeradkinggeo@gmail.com>
2024-09-27 12:35:58 -0500

    Minor UX change

--- erddap2agol/src/das_client.py
+++ erddap2agol/src/das_client.py
@@ -42,13 +42,13 @@ def parseDasResponse(response_text):
 def getConfDir():
 
     agol_home = os.getenv('AGOL_HOME', '/arcgis/home')
-    print(f"AGOL_HOME is set to: {agol_home}")
+    #print(f"AGOL_HOME is set to: {agol_home}")
 
     base_dir = agol_home
-    print(f"Base directory is: {base_dir}")
+    #print(f"Base directory is: {base_dir}")
 
     das_conf_dir = os.path.join(base_dir, 'das_conf')
-    print(f"DAS configuration directory is: {das_conf_dir}")
+    #print(f"DAS configuration directory is: {das_conf_dir}")
 
     os.makedirs(das_conf_dir, exist_ok=True)
     return das_conf_dir
